dd5bcd616dd804a47afd38e62470a2413fd8894e
roidrole <roidorle@gmail.com>
2025-10-24 07:44:35 -0400

    Fix NetherSteel's pick block

--- src/main/java/roidrole/tfutils/blocks/NetherSteel.java
+++ src/main/java/roidrole/tfutils/blocks/NetherSteel.java
@@ -2,10 +2,15 @@ package roidrole.tfutils.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
@@ -14,6 +19,8 @@ import java.util.List;
 public class NetherSteel extends Block {
 	public static Block BLOCK = new NetherSteel("");
 	public static ItemBlock ITEM = new NetherSteelItem("");
+
+	public Item pickItem;
 	public NetherSteel(String variant) {
 		super(Material.IRON);
 		this.setHardness(5.0f);
@@ -24,6 +31,11 @@ public class NetherSteel extends Block {
 			setRegistryName("bewitchment", "nethersteel_"+variant);
 		}
 		this.setTranslationKey("bewitchment.nethersteel");
+	}
+
+	@Override
+	public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player) {
+		return new ItemStack(pickItem, 1, 0);
 	}
 
 	public static class NetherSteelItem extends ItemBlock{
