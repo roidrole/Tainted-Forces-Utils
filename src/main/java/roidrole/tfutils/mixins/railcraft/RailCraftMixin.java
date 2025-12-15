7e1e39d91ead293829a32f135482637d41c90337
roidrole <roidorle@gmail.com>
2025-12-15 18:30:34 -0500

    Revert "Remove RailCraft mixins"
    This reverts commit f729d115560c2ca621be966825a983825d561ee5.

---
+++ src/main/java/roidrole/tfutils/mixins/railcraft/RailCraftMixin.java
@@ -0,0 +1,17 @@
+package roidrole.tfutils.mixins.railcraft;
+
+import mods.railcraft.common.core.Railcraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(Railcraft.class)
+public abstract class RailCraftMixin {
+	/**
+	 * @author roidrole
+	 * @reason remove that fingerprintviolation crash
+	 */
+	@Overwrite(remap = false)
+	public void fingerprintError(net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent event) {
+		//NO-OP
+	}
+}
