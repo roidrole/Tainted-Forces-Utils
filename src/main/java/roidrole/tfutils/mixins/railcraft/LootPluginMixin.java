5640316e08350ee6a18c003aa0c4a8ef9c242a73
roidrole <83431927+roidrole@users.noreply.github.com>
2026-02-22 08:25:15 -0500

    Deregister chest village workshop loot table

--- src/main/java/roidrole/tfutils/mixins/railcraft/LootPluginMixin.java
+++ src/main/java/roidrole/tfutils/mixins/railcraft/LootPluginMixin.java
@@ -1,8 +1,11 @@
 package roidrole.tfutils.mixins.railcraft;
 
 import mods.railcraft.common.plugins.forge.LootPlugin;
+import net.minecraft.util.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(LootPlugin.class)
 public abstract class LootPluginMixin {
@@ -13,5 +16,16 @@ public abstract class LootPluginMixin {
 	@Overwrite(remap = false)
 	public void init(){
 		//No-OP
+	}
+
+	@Redirect(
+		method = "<clinit>",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/world/storage/loot/LootTableList;func_186375_a(Lnet/minecraft/util/ResourceLocation;)Lnet/minecraft/util/ResourceLocation;"
+		)
+	)
+	private static ResourceLocation noRegistering(ResourceLocation resourceLocation){
+		return null;
 	}
 }
