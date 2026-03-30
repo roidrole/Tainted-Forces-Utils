2d83f39a167d86cca197294da4ee54b994560a3a
roidrole <83431927+roidrole@users.noreply.github.com>
2026-03-29 22:36:42 -0400

    Suction to Waila

---
+++ src/main/java/roidrole/tfutils/mixins/thaumicwaila/GogglesDisplayProviderMixin.java
@@ -0,0 +1,30 @@
+package roidrole.tfutils.mixins.thaumicwaila;
+
+import mcp.mobius.waila.api.IWailaConfigHandler;
+import mcp.mobius.waila.api.IWailaDataAccessor;
+import me.xfl03.thaumicwaila.provider.GogglesDisplayProvider;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import thaumcraft.common.lib.utils.EntityUtils;
+
+import java.util.List;
+
+@Mixin(GogglesDisplayProvider.class)
+public abstract class GogglesDisplayProviderMixin {
+	@Inject(
+		method = "getWailaBody",
+		at = @At(
+			value = "HEAD"
+		),
+		remap = false,
+		cancellable = true
+	)
+	private static void onlyIfGoggles(ItemStack itemStack, List<String> tooltip, IWailaDataAccessor accessor, IWailaConfigHandler config, CallbackInfoReturnable<List<String>> cir){
+		if(!EntityUtils.hasGoggles(accessor.getPlayer())){
+			cir.setReturnValue(tooltip);
+		}
+	}
+}
