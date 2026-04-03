209da83db666905be75f043f0428825e2a8982f3
roidrole <83431927+roidrole@users.noreply.github.com>
2026-04-03 09:32:41 -0400

    Nuke strengthened glass recipe

---
+++ src/main/java/roidrole/tfutils/mixins/railcraft/BlockStrengthGlassMixin.java
@@ -0,0 +1,18 @@
+package roidrole.tfutils.mixins.railcraft;
+
+import mods.railcraft.common.blocks.aesthetics.glass.BlockStrengthGlass;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(BlockStrengthGlass.class)
+public abstract class BlockStrengthGlassMixin {
+	/**
+	 * @author roidrole
+	 * @reason Original code gathers ItemStack capabilities, which is bad in this pack. Remaking it with CT
+	 * Saves 1.3 seconds of load time
+	 */
+	@Overwrite
+	public void defineRecipes(){
+		//No-op
+	}
+}
