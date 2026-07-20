4465a3b51ebc5ee92e76502f3bb40d3a48f0045c
roidrole <83431927+roidrole@users.noreply.github.com>
2026-07-20 12:25:07 -0400

    Fix leaf decay jankiness
    Recreate the default leaf decay algorithm, but make it actually efficient. This is a performance decrease, but the correctness increase is well worth it. The main performance issue with the original algorithm has been fixed

--- src/main/java/roidrole/tfutils/mixins/tfc/BlockLeavesTFCMixin.java
+++ src/main/java/roidrole/tfutils/mixins/tfc/BlockLeavesTFCMixin.java
@@ -1,11 +1,14 @@
 package roidrole.tfutils.mixins.tfc;
 
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.ints.IntList;
 import net.dries007.tfc.api.types.Tree;
 import net.dries007.tfc.client.particle.TFCParticles;
 import net.dries007.tfc.objects.blocks.wood.BlockLeavesTFC;
 import net.dries007.tfc.objects.blocks.wood.BlockLogTFC;
 import net.minecraft.block.BlockLeaves;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.*;
@@ -19,12 +22,20 @@ public abstract class BlockLeavesTFCMixin extends BlockLeaves {
 	@Final
 	public Tree wood;
 
+	//Queues of blockpos to visit, split and static to avoid excessive allocation
+	@Unique private static IntList tfutils_queueX1 = new IntArrayList();
+	@Unique private static IntList tfutils_queueY1 = new IntArrayList();
+	@Unique private static IntList tfutils_queueZ1 = new IntArrayList();
+	@Unique private static IntList tfutils_queueX2 = new IntArrayList();
+	@Unique private static IntList tfutils_queueY2 = new IntArrayList();
+	@Unique private static IntList tfutils_queueZ2 = new IntArrayList();
+
 	@Unique
 	private static final MutablerBlockPos tfutils_decayPos = new MutablerBlockPos();
 
 	/**
 	 * @author roidrole
-	 * @reason Optimize this shit
+	 * @reason Use a boolean[] instead of a HashSet for efficiency, lower memory allocation and temp object creation
 	 */
 	@Overwrite(remap = false)
 	private void doLeafDecay(World world, BlockPos posIn, IBlockState state)
@@ -32,34 +43,67 @@ public abstract class BlockLeavesTFCMixin extends BlockLeaves {
 		if (world.isRemote || !state.getValue(DECAYABLE)){
 			return;
 		}
+		int radius = wood.getMaxDecayDistance();
+		int sideSize = 2 * radius + 1;
 
-		//Scanning for a wood log
-		final BlockLogTFC log = BlockLogTFC.get(wood);
-		final int minDy = -wood.getMaxDecayDistance();
-		tfutils_decayPos.setPos(posIn);
-		tfutils_decayPos.addY(minDy - 1);
-		for (int dy = minDy; dy <= -minDy; dy++) {
-			tfutils_decayPos.addY();
-			int minDx = minDy + Math.abs(dy);
-			tfutils_decayPos.addX(minDx - 1);
-			for (int dx = minDx; dx <= -minDx; dx++) {
-				tfutils_decayPos.addX();
-				int minDz = minDx + Math.abs(dx);
-				tfutils_decayPos.addZ(minDz - 1);
-				for (int dz = minDz; dz <= -minDz; dz++) {
-					tfutils_decayPos.addZ();
+		boolean[] evaluated = new boolean[sideSize*sideSize*sideSize];
+		int minX = posIn.getX() - radius;
+		int minY = posIn.getY() - radius;
+		int minZ = posIn.getZ() - radius;
+
+		BlockLogTFC log = BlockLogTFC.get(wood);
+
+		tfutils_queueX1.clear();
+		tfutils_queueY1.clear();
+		tfutils_queueZ1.clear();
+		tfutils_queueX2.clear();
+		tfutils_queueY2.clear();
+		tfutils_queueZ2.clear();
+		tfutils_queueX1.add(posIn.getX());
+		tfutils_queueY1.add(posIn.getY());
+		tfutils_queueZ1.add(posIn.getZ());
+		for (int i = 1; i < radius; i++) {
+			for (int j = 0; j < tfutils_queueX1.size(); j++) {
+				int xOrigin = tfutils_queueX1.getInt(j);
+				int yOrigin = tfutils_queueY1.getInt(j);
+				int zOrigin = tfutils_queueZ1.getInt(j);
+
+				for(EnumFacing facing : EnumFacing.VALUES){
+					int x = xOrigin + facing.getXOffset();
+					int y = yOrigin + facing.getYOffset();
+					int z = zOrigin + facing.getZOffset();
+					int relPos = (x - minX) + (y - minY) * sideSize + (z - minZ) * sideSize * sideSize;
+					if(evaluated[relPos]){
+						continue;
+					}
+					tfutils_decayPos.setPos(x, y, z);
 					if(!world.isBlockLoaded(tfutils_decayPos)){
 						continue;
 					}
-					if(world.getBlockState(tfutils_decayPos).getBlock() == log){
+					IBlockState stateCheck = world.getBlockState(tfutils_decayPos);
+					if (stateCheck.getBlock() == log) {
 						return;
 					}
+					if (stateCheck.getBlock() == this) {
+						tfutils_queueX2.add(x);
+						tfutils_queueY2.add(y);
+						tfutils_queueZ2.add(z);
+					}
+					evaluated[relPos] = true;
 				}
-				tfutils_decayPos.addZ(minDz);
 			}
-			tfutils_decayPos.addX(minDx);
+			tfutils_queueX1.clear();
+			tfutils_queueY1.clear();
+			tfutils_queueZ1.clear();
+			//Swap queue 1 and 2. Since all 1 queue are equal (but must remain distinct), we can make this easier
+			IntList tempQueue = tfutils_queueZ1;
+			tfutils_queueZ1 = tfutils_queueZ2;
+			tfutils_queueZ2 = tfutils_queueY1;
+			tfutils_queueY1 = tfutils_queueY2;
+			tfutils_queueY2 = tfutils_queueX1;
+			tfutils_queueX1 = tfutils_queueX2;
+			tfutils_queueX2 = tempQueue;
 		}
-		//Not worth updating Y pos here
 
 		world.setBlockToAir(posIn);
 		int particleScale = 10;
