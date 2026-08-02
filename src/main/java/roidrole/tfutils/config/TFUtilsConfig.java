e400ecf4a16ba2ba60c29975563d60a9ace4a97b
roidrole <83431927+roidrole@users.noreply.github.com>
2026-08-02 11:41:03 -0400

    Remove unused config

--- src/main/java/roidrole/tfutils/config/TFUtilsConfig.java
+++ src/main/java/roidrole/tfutils/config/TFUtilsConfig.java
@@ -18,14 +18,6 @@ public class TFUtilsConfig {
 	@Config.Comment("How much mana the hydrogeanas will generate every 3 ticks (2 if raining)")
 	public static int hydrogenanasPowerMultiplier = 3;
 
-	public static float rockTaintificationThreashold = 0.2f;
-
-	@Config.Comment({
-		"Minimum value of taintEvo gamerule for taint to eat metals and glass.",
-		"rockTaintificationThreashold must also be respected for this to happen."
-	})
-	public static int metalMinEvo = 10000000;
-
 	@Config.Ignore
 	public static Aspect[] cloneAspects;
 
